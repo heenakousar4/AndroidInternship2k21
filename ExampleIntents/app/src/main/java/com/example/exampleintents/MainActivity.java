9871d2a8c039786f586ce1ade7055fb6e0b1ccd4
Heena Kousar <shaikheenakousar10@gmail.com>
2021-06-01 19:01:05 +0530

    added

---
+++ ExampleIntents/app/src/main/java/com/example/exampleintents/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.exampleintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //declare all views here
+    EditText name,number,link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name);
+        number=findViewById(R.id.number);
+        link=findViewById(R.id.link);
+    }
+
+    public void next(View view) {
+        String n = name.getText().toString();
+        Intent i = new Intent(this,SecondActivity.class);
+        i.putExtra("key",n);
+        startActivity(i);
+
+    }
+
+    public void dial(View view) {
+        String n = name.getText().toString();
+        Uri uri = Uri.parse("tel:"+n);
+        Intent i = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(i);
+    }
+
+    public void search(View view) {
+        String n = link.getText().toString();
+        Uri uri = Uri.parse("https://"+n);
+        Intent i = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(i);
+
+    }
+}
--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,24 +2,25 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-    //Declare the views here
+
     Button toast,count;
     TextView tv;
     int i = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Initialise the views here
+
         toast = findViewById(R.id.toast);
         count = findViewById(R.id.count);
         tv = findViewById(R.id.tv);
@@ -28,18 +29,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 i++;
                 tv.setText(""+i);
-               // tv.setText(String.valueOf(i));
+
+
             }
         });
         toast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
-                        "Welcome to Android.Your count is "+i,
-                        Toast.LENGTH_SHORT).show();
+                "Welcome to Android.YOur count is"+i,
+                Toast.LENGTH_SHORT).show();
+
             }
         });
-
     }
 
 }
