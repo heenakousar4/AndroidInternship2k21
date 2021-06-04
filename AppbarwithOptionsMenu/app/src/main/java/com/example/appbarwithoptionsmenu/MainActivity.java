a8112acb7e1564d7d81befb19fcd29e56a552d51
Heena Kousar <shaikheenakousar10@gmail.com>
2021-06-04 18:27:04 +0530

    added

---
+++ AppbarwithOptionsMenu/app/src/main/java/com/example/appbarwithoptionsmenu/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.appbarwithoptionsmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Resource >> access into java file (R.java is used)
+        //res >> to access into xml file(@-symbol)
+
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.name:
+                Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contact:
+                Toast.makeText(this,"Contact", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
